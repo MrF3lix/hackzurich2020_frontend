480011694de5ff74a41a7a33a72292e4b9a663d5
Felix Saaro <felix@saaro.ch>
2020-09-20 05:14:45 +0200

    Add animation

--- src/Dashboard/Map.js
+++ src/Dashboard/Map.js
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+import * as geolib from 'geolib';
 import ReactMapGL, { Marker } from 'react-map-gl';
 import { ConstructionSitePin, CementFactoryPin, TruckLocationPin, PickupLocationPin } from '../Atoms/Pin';
 import PolylineOverlay from './PolylineOverlay';
 
 export const Map = ({ viewport, setViewport, trucks, facilities, pickups, isDarkMode = false }) => {
-    const [activeItemId, setActiveItemId] = useState();
+    const [, setActiveItemId] = useState();
     const [showPickups, setShowPickups] = useState(true);
     const [showSites, setShowSites] = useState(true);
     const [showFactories, setShowFactories] = useState(true);
@@ -34,16 +35,6 @@ export const Map = ({ viewport, setViewport, trucks, facilities, pickups, isDark
                         }
                     </Marker>
                 ))}
-                {/* {facilities && facilities.map(facility => (
-                    <Marker key={facility.id} latitude={facility.locationLat} longitude={facility.locationLog}>
-                        {facility.id === activeItemId &&
-                            <div className="popup">
-                                <div>{facility.id} - {facility.type}</div>
-                                <div>{facility.locationLat}/{facility.locationLog}</div>
-                            </div>
-                        }
-                    </Marker>
-                ))} */}
 
                 {showPickups && pickups && Array.isArray(pickups) && pickups.map(pickup => (
                     <Marker onClick={() => updateActiveItem(pickup.locationLat, pickup.locationLog, pickup.id)} key={pickup.id} latitude={pickup.locationLat} longitude={pickup.locationLog}>
@@ -52,14 +43,33 @@ export const Map = ({ viewport, setViewport, trucks, facilities, pickups, isDark
                 ))}
                 {showTrucks && trucks && Array.isArray(trucks) && trucks.map(truck => {
                     let route = [];
+                    let direction = 0;
                     if(truck.route) {
                         route = JSON.parse(truck.route);
+
+                        let index = route.findIndex(item => item[0] === truck.currentLocationLon && item[1] === truck.currentLocationLat);
+                        console.log(index);
+
+                        let nextIndex = index === route.length-1 ? index-1:index+1;
+
+                        direction = geolib.getRhumbLineBearing(
+                            {
+                                latitude: route[index][1],
+                                longitude: route[index][0]
+                            },
+                            {
+                                latitude: route[nextIndex][1],
+                                longitude: route[nextIndex][0]
+                            }
+                        );
+
+                        console.log(direction);
                     }
                     return (
                         <React.Fragment key={truck.id}>
                             <PolylineOverlay points={route}/>
-                            <Marker latitude={truck.currentLocationLat} longitude={truck.currentLocationLon}>
-                                <TruckLocationPin size={35} direction={truck.angle} onClick={() => updateActiveItem(truck.currentLocationLat, truck.currentLocationLon, truck.id)} />
+                            <Marker latitude={truck.currentLocationLat} longitude={truck.currentLocationLon} className="mapboxgl-marker--animated">
+                                <TruckLocationPin size={35} direction={direction} onClick={() => updateActiveItem(truck.currentLocationLat, truck.currentLocationLon, truck.id)} />
                             </Marker>
                         </React.Fragment>
                     );
